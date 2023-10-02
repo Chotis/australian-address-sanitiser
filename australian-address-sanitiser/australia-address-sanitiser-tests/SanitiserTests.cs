0a0a53c71fc1aa79a8a00da46d5f8c9107c54d07
Panayiotis Moutzouris <elgreco@live.co.uk>
2023-10-02 13:04:15 +1100

    Added more test cases

--- australian-address-sanitiser/australia-address-sanitiser-tests/SanitiserTests.cs
+++ australian-address-sanitiser/australia-address-sanitiser-tests/SanitiserTests.cs
@@ -1,4 +1,6 @@
 using australian_address_sanitiser;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
 
 namespace australia_address_sanitiser_tests
 {
@@ -12,9 +14,16 @@ namespace australia_address_sanitiser_tests
         [InlineData("123 Test road, Croydon, NSW 2132", "123 Test Road, Croydon, NSW 2132")]
         [InlineData("123/456 Test Rd, Oakleigh, VIC 3166", "123/456 Test Rd, Oakleigh, VIC 3166")]
         [InlineData("123-456 Test    Rd,   Hallam, VIC 3803", "123-456 Test Rd, Hallam, VIC 3803")]
+        [InlineData("10 - 20 Test Drive, Tweed Heads South, NSW 2486", "10-20 Test Drive, Tweed Heads South, NSW 2486")]
+        [InlineData("123 / 456 Test Rd, Oakleigh, VIC 3166", "123/456 Test Rd, Oakleigh, VIC 3166")]
+        [InlineData("1 / 234 - 567 Test Rd, Braeside, VIC 3195", "1 /  234  -   567 Test Rd, Braeside, VIC 3195")]
+        [InlineData("1­--5 Test Street, Tamworth, NSW 2340", "1­ 5 Test Street, Tamworth, NSW 2340")]
+        [InlineData("Building 2,70 Test Street, Braybrook, VIC 3019", "Building 2,70 Test St, Braybrook, VIC 3019")]
+        [InlineData("Cnr Test Street and Test Street, Heidelberg, VIC 3084", "Corner Test Street and Test Street, Heidelberg, VIC 3084")]
+        [InlineData("P.O.Box 123, Test, QLD 4557", "POBox 123, Test, QLD 4557")]
+        [InlineData("Yards 1,2,3,4.  567 Test Avenue, Minchinbury, NSW 2770", "Yards 1.2.3,4. 567 Test Avenue, Minchinbury, NSW 2770")]
         public void SanitiseAddress_SimilarAddress_Match(string address, string similarAddress)
         {
-            
             //Act
             var firstAddress = _sut.SanitizeAddress(address);
             var secondAddress = _sut.SanitizeAddress(similarAddress);
@@ -29,9 +38,16 @@ namespace australia_address_sanitiser_tests
         [InlineData("123 Test road, Croydon, NSW 2132", "123 Test Road, Croydon, NSW 2132")]
         [InlineData("123/456 Test Rd, Oakleigh, VIC 3166", "123/456 Test Rd, Oakleigh, VIC 3166")]
         [InlineData("123-456 Test    Rd,   Hallam, VIC 3803", "123-456 Test Rd, Hallam, VIC 3803")]
+        [InlineData("10 - 20 Test Drive, Tweed Heads South, NSW 2486", "10-20 Test Drive, Tweed Heads South, NSW 2486")]
+        [InlineData("123 / 456 Test Rd, Oakleigh, VIC 3166", "123/456 Test Rd, Oakleigh, VIC 3166")]
+        [InlineData("1 / 234 - 567 Test Rd, Braeside, VIC 3195", "1 /  234  -   567 Test Rd, Braeside, VIC 3195")]
+        [InlineData("1­--5 Test Street, Tamworth, NSW 2340", "1­ 5 Test Street, Tamworth, NSW 2340")]
+        [InlineData("Building 2,70 Test Street, Braybrook, VIC 3019", "Building 2,70 Test St, Braybrook, VIC 3019")]
+        [InlineData("Cnr Test Street and Test Street, Heidelberg, VIC 3084", "Corner Test Street and Test Street, Heidelberg, VIC 3084")]
+        [InlineData("P.O.Box 123, Test, QLD 4557", "POBox 123, Test, QLD 4557")]
+        [InlineData("Yards 1,2,3,4.  567 Test Avenue, Minchinbury, NSW 2770", "Yards 1.2.3,4. 567 Test Avenue, Minchinbury, NSW 2770")]
         public void SanitiseAddress_SimilarAddressAdditionalParameters_Match(string address, string similarAddress)
         {
-
             //Act
             var firstAddress = _sut.SanitizeAddress(address, false, false, true);
             var secondAddress = _sut.SanitizeAddress(similarAddress, false, false, true);
