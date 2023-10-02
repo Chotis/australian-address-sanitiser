349561af57d9e1f021954fb90c180ecd8c1338e9
Panayiotis Moutzouris <elgreco@live.co.uk>
2023-10-02 12:29:10 +1100

    Added test to test optional parameters

--- australian-address-sanitiser/australia-address-sanitiser-tests/SanitiserTests.cs
+++ australian-address-sanitiser/australia-address-sanitiser-tests/SanitiserTests.cs
@@ -22,5 +22,22 @@ namespace australia_address_sanitiser_tests
             //Assert
             Assert.Equal(firstAddress, secondAddress);
         }
+
+        [Theory]
+        [InlineData("123 Test Highway, Ferntree Gully, VIC 3156", "123 Test Hwy, Ferntree Gully, VIC 3156")]
+        [InlineData("123 Test Rd, Coopers Plains, QLD 4108", "123 test Road, Coopers Plains, QLD 4108")]
+        [InlineData("123 Test road, Croydon, NSW 2132", "123 Test Road, Croydon, NSW 2132")]
+        [InlineData("123/456 Test Rd, Oakleigh, VIC 3166", "123/456 Test Rd, Oakleigh, VIC 3166")]
+        [InlineData("123-456 Test    Rd,   Hallam, VIC 3803", "123-456 Test Rd, Hallam, VIC 3803")]
+        public void SanitiseAddress_SimilarAddressAdditionalParameters_Match(string address, string similarAddress)
+        {
+
+            //Act
+            var firstAddress = _sut.SanitizeAddress(address, false, false, true);
+            var secondAddress = _sut.SanitizeAddress(similarAddress, false, false, true);
+
+            //Assert
+            Assert.Equal(firstAddress, secondAddress);
+        }
     }
 }
